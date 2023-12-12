36709ed845e80aafbe00fce0f783c6cb2c4aded1
Satyam Singh <143794249+Satyamlpu@users.noreply.github.com>
2023-12-12 13:39:48 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,41 @@
+# a= int(input("enter no"))
+# if a%2==0:
+    # print("even")
+# else:
+    # print("odd")
+    
+    
+# str = input()
+# print("good morning")
+
+# a = int(input("principle"))
+# b = int(input("rate"))
+# c = int(input("time"))
+# 
+# formula = (a*b*c)/100
+# print(formula)
+
+# a = int(input())
+# b = int(input())
+# if a>b:
+    # print("f")
+# else:
+    # print("g")
+    
+# a = int(input())
+# b = a*84
+# print(b)
+
+# a = 5>4>3>7>2>1
+# print(a)
+
+# def multiply(x, y):
+    # print(x * y)
+# multiply(3,5)
+ 
+tup = (1, 2, 3, 4, 5)
+print(len(tup))
+print(tup[-1])
+print(tup[-3])
+if 5 in tup:
+    print("yes")
